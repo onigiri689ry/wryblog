85e302b1dc122b9ff2f7dc8cabc55b513092fe4e
onigiri689ry <59249071+onigiri689ry@users.noreply.github.com>
2020-02-22 21:04:49 +0900

    routes追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:'blogs#index'
-  resources :blogs, only:[:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :blogs do
+    resources :comments, only: :create
+  end
   resources :tags, only:[:index, :show]
   resources :tagmaps, only:[:index,:show]
   resources :users, only: :show
