a7498371c2f91db23a20e83e5e385e210953b7fd
onigiri689ry <59249071+onigiri689ry@users.noreply.github.com>
2020-02-22 15:02:11 +0900

    blogテーブル作成

---
+++ db/migrate/20200222055936_create_blogs.rb
@@ -0,0 +1,10 @@
+class CreateBlogs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.text :body
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
