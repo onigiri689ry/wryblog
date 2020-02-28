da28d708494ca593ad4ee27f8dc37b955959e664
onigiri689ry <59249071+onigiri689ry@users.noreply.github.com>
2020-02-28 09:35:52 +0900

    js途中

---
+++ app/assets/javascripts/blogs.js
@@ -0,0 +1,6 @@
+// var box = document.getElementsByClassName("box10");
+// var btn = document.getElementsByClassName("button1");
+
+$("button1").addEventListener("click", function () {
+  $("box10").toggleClass('active1');
+});
