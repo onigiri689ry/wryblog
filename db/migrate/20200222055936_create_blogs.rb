36f5a3b20116c288992d57e42e60bfee059cb247
onigiri689ry <59249071+onigiri689ry@users.noreply.github.com>
2020-03-01 17:10:11 +0900

    db作成、編集

--- db/migrate/20200222055936_create_blogs.rb
+++ db/migrate/20200222055936_create_blogs.rb
@@ -1,8 +1,8 @@
 class CreateBlogs < ActiveRecord::Migration[5.0]
   def change
     create_table :blogs do |t|
-      t.string :title, null: false
-      t.text :body, null: false
+      t.string :title, 　null: false
+      t.text :body, 　　　null: false
       t.integer :user_id
       t.timestamps
     end
