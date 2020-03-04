9bd9022ba4443a179a9784e8342fbdd7b61d0810
onigiri689ry <59249071+onigiri689ry@users.noreply.github.com>
2020-03-04 13:55:23 +0900

    slack調整

---
+++ app/assets/javascripts/blogs.index.js
@@ -0,0 +1,27 @@
+$(function () {
+  $('.slick').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 2,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
--- app/assets/javascripts/blogs.js
+++ app/assets/javascripts/blogs.js
@@ -69,31 +69,5 @@ $(function () {
     $(this).parent().parent().css('opacity', '0');
     return false;
   });
-
-  $('.slick').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 2,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
 });
 
